fd95ed25b6e27894a4db28c9c7fc8e85c43f22a9
Claes Wallin (韋嘉誠) <claes.wallin@greatsinodevelopment.com>
2018-11-26 15:59:28 +0800

    WIP: bolos/ledger: experiments in skipping the big binary deps

--- ledger/bolos-env.nix
+++ ledger/bolos-env.nix
@@ -1,21 +1,14 @@
 { pkgs ? import ../nix/nixpkgs.nix {}
+, pkgsCross ? pkgs.pkgsCross
 , fetchurl ? pkgs.fetchurl
 , runCommand ? pkgs.runCommand
 }:
 
 let
-  clang = fetchurl {
-    url = http://releases.llvm.org/4.0.0/clang+llvm-4.0.0-x86_64-linux-gnu-ubuntu-16.10.tar.xz;
-    sha256 = "0j0kc73xvm2dl84f7gd2kh6a8nxlr7alk91846m0im77mvm631rv";
-  };
-  gcc = fetchurl {
-    url = https://launchpadlibrarian.net/251687888/gcc-arm-none-eabi-5_3-2016q1-20160330-linux.tar.bz2;
-    sha256 = "08x2sv2mhx3l3adw8kgcvmrs10qav99al410wpl18w19yfq50y11";
-  };
+  clang = pkgsCross.raspberryPi.buildPackages.clang_4;
+  gcc = pkgsCross.raspberryPi.buildPackages.gcc5;
 in runCommand "bolos-env" {} ''
   mkdir $out
-  mkdir $out/clang-arm-fropi
-  tar xaf ${clang} --strip-components=1 -C $out/clang-arm-fropi
-  mkdir $out/gcc-arm-none-eabi-5_3-2016q1
-  tar xaf ${gcc} --strip-components=1 -C $out/gcc-arm-none-eabi-5_3-2016q1
+  ln -s ${clang} $out/clang-arm-fropi
+  ln -s ${gcc} $out/gcc-arm-none-eabi-5_3-2016q1
 ''
